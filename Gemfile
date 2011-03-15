a8e54406092155db143c22941260c016094d0e5e
Jose Ghislain Quenum <josum@me.com>
2011-03-15 13:24:17 +0100

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'http://rubygems.org'
+
+gem 'rails', '>= 3.0.5'
+gem 'mongoid', '>= 2.0.0.rc.7'
+gem 'bson_ext', '>=1.2.4'
+gem 'devise', '>= 1.2.rc'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Harare::Application.config.secret_token = '7654658f1efe758e904272cd40192f05591c2f76ae9bad832c480b99ff8f72da26654fd219d2acacc834be4d62f12b9e34cb42e20392e7a2e94d3832c8aeedd7'
